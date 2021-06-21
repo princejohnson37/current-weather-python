a6782229379c1ef0e09bb32db8b917a87d69a328
Prince Johnson <35042040+princejohnson37@users.noreply.github.com>
2021-06-22 02:04:40 +0530

    added api feature

--- weather.py
+++ weather.py
@@ -1,27 +1,33 @@
 import requests
 import datetime
-import api_config #file made by me, to store api key
 import json
+
 def try_another_city():
     choice = input(str("Do you want to search for weather of another city?(Yes/no)")).lower()
-    if(choice=='yes' or choice == 'y'):
+    if(choice == 'yes' or choice == 'y'):
         return True
     else:
         print("Exiting...")
         return False
 
+def enter_api():
+    api = input("Enter api: ")
+    return api
 
-api_key = api_config.api #api key is hidden for security purposes
+api_key = enter_api() 
 flag=True
+
 while(flag == True):
     city = input('Enter city: ')
     api_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city,api_key)
+
     try :
         response = requests.get(api_url)
         #print(response.status_code)
         #print(type(response.status_code))
+
         if response.status_code == 200 :
-            print('Connection Successful............')
+            print('Connection Successful...')
             content = response.json()
             date = datetime.datetime.now()
             location = city
@@ -30,17 +36,23 @@ while(flag == True):
             humidity = content['main']['humidity']
             wind_speed = content['wind']['speed']
             print(' date: {}\n location: {}\n temperature: {}\n weather: {}\n humidity: {}\n wind_speed: {}'.format(date,location,temp,weather,humidity,wind_speed))
+            
             try :
                 f = open("result.txt", "a+")
                 f.write('date: {}\nlocation: {}\ntemperature: {}\nweather: {}\nhumidity: {}\nwind_speed: {}\n'.format(date,location,temp,weather,humidity,wind_speed))
                 print('result.txt file contains the output\n')
                 f.close()
                 flag = try_another_city()
+
             except Exception as file_error :
                 print("FILE ERROR: {}".format(file_error))
+                flag = try_another_city()
+
         else:
             print("Status Code: {}, {}".format(response.status_code,response.reason))
+            flag = try_another_city()
 
     except Exception as error:
-        print("ERROR1:{}".format(error))
+        print("ERROR:{}".format(error))
+        flag = try_another_city()
 
